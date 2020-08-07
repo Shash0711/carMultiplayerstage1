489938d7476e20c8268db2c867137c97af18c89f
Shashwat Malhotra <shash200711@gmail.com>
2020-08-07 20:49:47 +0530

    multiplayer demo

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,33 @@ class Game {
       form.display();
     }
   }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Gooo!", 120, 100);
+    Player.getPlayerInfo();
+    
+    if(allPlayers !== undefined){
+      var display_position = 130;
+      for(var plr in allPlayers){
+        if(plr === "player" + player.index){
+          fill("green");
+        }else{
+          fill("black");
+        }
+
+        display_position += 20;
+        textSize(15);
+        text(allPlayers[plr].name + "; "+ allPlayers[plr].distance, 120, display_position);
+        
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) || keyIsDown("W") && player.index !== null){
+      player.distance += 50;
+      player.update();
+      
+    }
+  }
 }
